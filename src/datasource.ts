da2408b0cf3fd0c214715d1a9560eb1812ec1f7b
Marty Schoch <marty.schoch@gmail.com>
2023-05-26 14:58:15 -0400

    switch to using call resource functionality
    previously we did schema lookup using the query mechanism
    grafana plugins support another interface to do general lookups
    this switches to use that mechanism

--- src/datasource.ts
+++ src/datasource.ts
@@ -24,37 +24,6 @@ export class DataSource extends DataSourceWithBackend<AxiomQuery, AxiomDataSourc
     };
   }
 
-  async lookupSchema() {
-    const schemaQuery: AxiomQuery = {
-      apl: "",
-      totals: false,
-      refId: "a",
-      queryType: "schemaLookup"
-    };
-    const request = {
-      targets: [
-        {
-          ...schemaQuery,
-          refId: 'metricFindQuery',
-        },
-      ],
-    } as DataQueryRequest<AxiomQuery>;
-
-    let res: DataQueryResponse | undefined;
-
-    try {
-      res = await this.query(request).toPromise();
-    } catch (err) {
-      return Promise.reject(err);
-    }
-
-    if (res && (!res.data.length || !res.data[0].fields.length)) {
-      return [];
-    }
-
-    return res ? res.data[0].fields[0].values.buffer : {};
-  }
-
   async metricFindQuery(query: AxiomQuery, options?: any) {
     const request = {
       targets: [
@@ -80,6 +49,10 @@ export class DataSource extends DataSourceWithBackend<AxiomQuery, AxiomDataSourc
     }
 
     return res ? (res.data[0] as DataFrame).fields[0].values.toArray().map((_) => ({ text: _.toString() })) : [];
+  }
+
+  async lookupSchema() {
+    return this.getResource('/schema-lookup');
   }
 
 }
